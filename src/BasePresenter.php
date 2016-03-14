e5ff4dac97ea2e640c6ccce2ce2d208219cc1f5a
DraperStudio <brideloper@gmail.com>
2016-03-14 07:35:43 +0100

    Create BasePresenter.php

---
+++ src/BasePresenter.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Eloquent Presenter.
+ *
+ * (c) DraperStudio <hello@draperstudio.tech>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace DraperStudio\Eloquent\Presenter;
+
+/**
+ * Class BasePresenter.
+ *
+ * @author DraperStudio <hello@draperstudio.tech>
+ */
+class BasePresenter extends AbstractPresenter
+{
+    use Traits\RoutesTrait;
+    use Traits\DateTimeTrait;
+}
