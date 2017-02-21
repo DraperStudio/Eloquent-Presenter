bcbc6ac4a8b10559cb32b20a2a3a541cb0fe5f97
Brian Faust <faustbrian@users.noreply.github.com>
2017-02-21 15:28:04 +0200

    Use dmY as default date format

--- src/Traits/DateTimeTrait.php
+++ src/Traits/DateTimeTrait.php
@@ -32,7 +32,7 @@ trait DateTimeTrait
      *
      * @return string
      */
-    public function createdAt($timeAgo = false, $format = 'l jS \\of F Y'): string
+    public function createdAt($timeAgo = false, $format = 'd.m.Y'): string
     {
         return $timeAgo ? $this->getTimeAgo($this->created_at) : $this->created_at->format($format);
     }
@@ -43,7 +43,7 @@ trait DateTimeTrait
      *
      * @return string
      */
-    public function updatedAt($timeAgo = false, $format = 'l jS \\of F Y'): string
+    public function updatedAt($timeAgo = false, $format = 'd.m.Y'): string
     {
         return $timeAgo ? $this->getTimeAgo($this->updated_at) : $this->updated_at->format($format);
     }
@@ -54,7 +54,7 @@ trait DateTimeTrait
      *
      * @return string
      */
-    public function deletedAt($timeAgo = false, $format = 'l jS \\of F Y'): string
+    public function deletedAt($timeAgo = false, $format = 'd.m.Y'): string
     {
         return $timeAgo ? $this->getTimeAgo($this->deleted_at) : $this->deleted_at->format($format);
     }
